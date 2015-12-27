913c0d7a8a9d28b8482ec38b7f319e8454580d83
dinosaurfiles <dinosaurfiles@users.noreply.github.com>
2015-12-27 17:51:13 +0800

    v0.1 Beta Version

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# cybrary-video-downloader (Experimental)
-Downloads Course Videos From [Cybrary](Cybrary.it) (For Educational Purpose only)
+# cybrary-video-downloader (Beta)
+Downloads Course Videos From [Cybrary.it](https://www.cybrary.it/) (For Educational Purpose only)
 ## Why u do diz?
 This program is made for the love and passion for cybersecurity. Also for a friend who has a slow internet connection and the needs for offline learning
 ## For the folks at Cybrary.it
@@ -11,5 +11,13 @@ I made this program for the sole purpose that I mentioned above and kudos for al
 - `youtube-dl` - Follow this [guide](https://rg3.github.io/youtube-dl/download.html) to install youtube-dl
 - `BeautifulSoup` - Installation instructions [here](http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup)
 
-
 ### Windows (Soon..)
+
+## Usage:
+Just run `python cybrary-video-downloader.py`
+
+
+### Versions:
+| Version  | Release Date | Description| Known Bugs |
+| ------------- | ------------- | ------------- | ------------- |
+| `v0.1`  | 12-07-15  | Beta  | <ul><li>No proper Authentication. User must input the right credentials</li><li>Downloading of videos are not in order</li></ul> |
--- cybrary-video-downloader.py
+++ cybrary-video-downloader.py
@@ -1,24 +1,26 @@
 import re
 import sys
 import urllib
+import getpass
 import urllib2
 import requests
 import cookielib
 from bs4 import BeautifulSoup
+from subprocess import call
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-#Global Variables
+### Global Variables
 courselist = {};
 courseselectionlist = {};
 selectedcourse = None
 selectedcourselink = None
-videoquality = None
+videoquality = 360
+session = requests.session()
 
+### Session Login, Get Courses
 def login(username, passwd):
-    session = requests.session()
-
     values = {
         'log': username,
         'pwd': passwd,
@@ -27,17 +29,15 @@ def login(username, passwd):
         'redirect_to': ''
     }
 
-    r = session.post('https://www.cybrary.it/wp-login.php', data=values)
-    r = session.get('https://www.cybrary.it/courses/')
+    sessionlink = session.post('https://www.cybrary.it/wp-login.php', data=values)
+    sessionlink = session.get('https://www.cybrary.it/courses/')
 
-    opentxt=open('course.txt','r')
-    readtxt=opentxt.read()
+    get_course_list(sessionlink.text)
 
-    get_course_list(r.text)
-
+### Get Courses List
 def get_course_list(html):
-    find = re.compile('<a href="https?://www.cybrary.it/course/\w+(?:-[\w]+)*/">(?:\w|\+| )+</a>')
-    coursematch = list(set(find.findall(html)))
+    coursesregex = re.compile('<a href="https?://www.cybrary.it/course/\w+(?:-[\w]+)*/">(?:\w|\+| )+</a>')
+    coursematch = list(set(coursesregex.findall(html)))
     for i in range (len(coursematch)):
         getelements = BeautifulSoup(coursematch[i], 'html.parser')
         getelements = getelements.find_all('a')
@@ -46,6 +46,7 @@ def get_course_list(html):
             courselink = link.get('href')
             courselist[coursename] = courselink
 
+### Select A Course
 def courseselection():
     print " Courses Available:"
     a = 0
@@ -60,46 +61,55 @@ def courseselection():
         selectedcourse = input(" Select a Course to Download: ")
 
     print "\n\n Downloading Course Videos of",courseselectionlist[selectedcourse]
+    global selectedcourselink
     selectedcourselink = courselist[courseselectionlist[selectedcourse]]
     print " Course Link:",selectedcourselink
 
+### Select Video Quality
 def selectquality():
-    quality = input("\n Select Video Quality: 360p/720p: ")
+    quality = input("\n Select Video Quality: 360/720: ")
 
     while quality != 360 and quality != 720 :
         print " Error! Select an appropriate video quality"
         quality = input("\n Select Video Quality: 360p/720p: ")
+    global videoquality
     videoquality = quality
 
+### Get Links of Each Module
 def getmodules():
-    #r = session.get(selectedcourselink)
-    #readtxt = r.text
-    opentxt1=open('testselectedcourse.txt','r')
-    readtxt1=opentxt1.read()
-    #print readtxt1
-    moduletable = BeautifulSoup(readtxt1, 'html.parser')
+    modulehtml = session.get(selectedcourselink)
+    moduletable = BeautifulSoup(modulehtml.text, 'html.parser')
     modulediv = moduletable.find('div', class_="modulelisting")
-    test0 = str(modulediv)
+    divhtml = str(modulediv)
 
-    find = re.compile('https?://www.cybrary.it/video/\w+(?:-[\w]+)*/')
-    modulematch = list(set(find.findall(test0)))
+    moduleregex = re.compile('https?://www.cybrary.it/video/\w+(?:-[\w]+)*/')
+    modulematch = list(set(moduleregex.findall(divhtml)))
     for i in range (len(modulematch)):
-        print modulematch[i]
-        #Download Video here
+        print " Downloading Video of",modulematch[i]
+        videodownload(str(modulematch[i]))
 
+### Download Videos using youtube-dl
+def videodownload(url):
+    video = session.get(url)
+    videohtml = video.text
+
+    #https://player.vimeo.com/video/999999999
+    videoregex = re.search( r'https://player.vimeo.com/video/\w*', videohtml)
+    if videoregex:
+        command = "youtube-dl -cif http-%sp %s" % (videoquality, videoregex.group())
+        call(command.split(), shell=False)
+    else:
+        print "Something is wrong. Please contact dinosaurfiles on Github"
 
 def main():
-    #username = raw_input("Enter Username: ")
-    #passwd = raw_input("Enter Your Password: ")
-    #login(username,passwd)
-    opentxt=open('testcourse.txt','r')
-    readtxt=opentxt.read()
-    #get_course_list(readtxt)
-    #courseselection()
-    #selectquality()
+    username = raw_input("Enter Username: ")
+    passwd = getpass.getpass()
+    login(username,passwd)
+    courseselection()
+    selectquality()
     getmodules()
 
-    #exit = raw_input("Exit")
+    exit = raw_input("Download Finished")
 
 if __name__ == '__main__':
 	main()
